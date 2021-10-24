f09e2e34d98c73f1e6ca8ab42db9064ddd53caab
sharathr25 <sharathdupati@gmail.com>
2021-10-24 18:42:57 +0530

    prod url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,11 @@
 const host = '192.168.1.33:5000' // host address when connected to wifi to check in mobile
 // const host = 'localhost:5000'
-export const WEBSOCKET_URL = `ws://${host}/socket`
+export const WEBSOCKET_URL =
+  process.env.NODE_ENV === 'development'
+    ? `ws://${host}/socket`
+    : 'ws://thawing-waters-18867.herokuapp.com/'
 
-export const API_URL = `http://${host}:5000`
+export const API_URL =
+  process.env.NODE_ENV === 'development'
+    ? `http://${host}:5000`
+    : 'https://thawing-waters-18867.herokuapp.com/'
